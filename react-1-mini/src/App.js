218a1bdde95bc7a9d9db4ef7097dbb644843d9dd
Valdez <Justin.Valdez.Contractor@pepsico.com>
2021-04-19 17:55:22 -0600

    Done, need to figure out pictures acaully showing up, vs broken lins

--- react-1-mini/src/App.js
+++ react-1-mini/src/App.js
@@ -9,32 +9,58 @@ class App extends Component {
       picture:"",
       name:""
     };
+    
 
     this.updatePicture = this.updatePicture.bind(this)
     this.updateName = this.updateName.bind(this)
+    this.addFriend = this.addFriend.bind(this)
+    
   };
   updatePicture(value){
     this.setState({picture : value});
-  };
+  }
   updateName(value){
     this.setState({name : value}); 
-  };
+  }
 
-addFriend(value){
-  this.setState({friends:value});  //START AGAIN HERE
-};
+  addFriend(){
+  const {friends, picture, name} =  this.state ;
+  
+  let newFriends = friends.slice();
+  newFriends.push({picture,name});
+
+  this.setState({friends: newFriends, picture: '', name: ''});
+  }
 
   render(){
+    console.log(this.state.name);
+    console.log(this.state.picture)
+    console.log(this.state.friends);
+
+  const friends = this.state.friends.map((friend, index)=>(
+    <div key={ `friend-${index}-${friend.name}`}>
+      <img width="100px" src={friend.picture} alt={''} />
+      <span>{friend.name}</span>
+    </div>
+  ));
+
   return (
     <div>
     <span>Pictures:</span>
     <input
-    onChange={( e ) => this.updatePicture( e.target.value ) } value={ this.state.picture }
+    onChange={( e ) => this.updatePicture( e.target.value ) } 
+    value={ this.state.picture }
      />
     <span>Name:</span> 
     <input
-    onChange={( e ) => this.updateName( e.target.value ) } value={ this.state.name } />
-    <button>Add Friend</button>
+    onChange={( e ) => this.updateName( e.target.value ) } 
+    value={ this.state.name } 
+    />
+
+    <button onClick={()=>{this.addFriend()}}>Add Friend</button>
+
+    {friends}
+
     </div>
        );
       };
